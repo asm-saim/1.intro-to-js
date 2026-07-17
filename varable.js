5313305b427dbec59cda0ad4ba5929450ecfae93
asm-saim <saim777m@gmail.com>
2026-07-17 09:43:21 +0600

    first commit

---
+++ varable.js
@@ -0,0 +1,12 @@
+let price = 34;
+price = 89;
+price = 78;
+const name = "Saidul";
+
+console.log(price, name);
+
+
+const a = 0.1;
+const b = 0.2;
+const total = (a + b).toFixed(2);
+console.log(total)
