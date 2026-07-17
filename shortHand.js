56e92fce81fd03a417c48a9d6fbcb9ca56a800d7
asm-saim <saim777m@gmail.com>
2026-07-17 11:14:47 +0600

    shorthand implemented

---
+++ shortHand.js
@@ -0,0 +1,12 @@
+let a = 30;
+let b = 20;
+
+a += 10; //a=a+10
+b -= 10; //b=b-10
+
+// 1 increment, 1 decrement
+a++;
+b--;
+//if we want to use console as , increment or decrement 
+console.log(++a);
+console.log(--b)
